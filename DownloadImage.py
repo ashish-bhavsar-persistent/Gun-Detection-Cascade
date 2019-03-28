58090a0d587f80317be22afebabb1b8ea2d71852
Ashishkumar Bhavsar <ashishkumar_bhavsar@persistent.co.in>
2019-03-28 10:54:04 +0530

    added threading logic

--- DownloadImage.py
+++ DownloadImage.py
@@ -1,29 +1,39 @@
+import time
+from threading import Thread
 import urllib.request
 import cv2
 import numpy as np
 import os
+import uuid
+import  threading
 
+def sleeper(i):
+    print(i)
+    try:
+        print(i)
+        pic_num = uuid.uuid1()
+        print(pic_num)
+
+        request = urllib.request.urlopen(i, timeout=50)
+        with open("neg/" + str(pic_num) + ".jpg", 'wb') as f:
+            f.write(request.read())
+        # urllib.request.urlretrieve(i,'neg/'+str(pic_num)+'.jpg')
+        img = cv2.imread("neg/" + str(pic_num) + ".jpg", cv2.IMREAD_GRAYSCALE)
+        resized_image = cv2.resize(img, (100, 100))
+        cv2.imwrite('neg/' + str(pic_num) + '.jpg', resized_image)
+        pic_num += 1
+    except Exception as e:
+        print(e)
 def store_raw_images():
-    neg_images_link ='http://image-net.org/api/text/imagenet.synset.geturls?wnid=n00523513'
+    neg_images_link = 'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n00523513'
     neg_image_urls = urllib.request.urlopen(neg_images_link).read().decode()
     if not os.path.exists('neg'):
         os.makedirs('neg')
-    pic_num = 1
-    img_count = 0
+    activeCount = 0
     for i in neg_image_urls.split('\n'):
-        try:
-            print(i)
-            img_count += 1
-            print(str(img_count))
-            request = urllib.request.urlopen(i, timeout=50)
-            with open("neg/"+str(pic_num)+".jpg", 'wb') as f:
-                f.write(request.read())
-            # urllib.request.urlretrieve(i,'neg/'+str(pic_num)+'.jpg')
-            img = cv2.imread("neg/"+str(pic_num)+".jpg", cv2.IMREAD_GRAYSCALE)
-            resized_image = cv2.resize(img,(100,100))
-            cv2.imwrite('neg/'+str(pic_num)+'.jpg', resized_image)
-            pic_num+=1
-        except Exception as e:
-            print(e)
-    print('Done')
+        t = Thread(target=sleeper, args=(i,))
+        t.start()
+        if  threading.active_count()>150 :
+            time.sleep(20)
+
 store_raw_images()
