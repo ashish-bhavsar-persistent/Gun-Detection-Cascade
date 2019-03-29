318c17fbfcfaefeb2670c090ad6b9a60402f0316
ashishkumar_bhavsar <ashishkumar_bhavsar@persistent.com>
2019-03-29 11:09:15 +0530

    Added New Negative Image Data Set

--- Resize Image.py
+++ Resize Image.py
@@ -3,16 +3,16 @@ import os
 
 def resize():
 
-        # for file_type in ['neg']:
-        #     for img in os.listdir(file_type):
-        #         current_image_path = str(file_type) + "/" + str(img)
-        #         img1 = cv2.imread(current_image_path, cv2.IMREAD_GRAYSCALE)
-        #         resized_image = cv2.resize(img1, (630, 360))
-        #         cv2.imwrite(current_image_path, resized_image)
+        for file_type in ['neg']:
+            for img in os.listdir(file_type):
+                current_image_path = str(file_type) + "/" + str(img)
+                img1 = cv2.imread(current_image_path, cv2.IMREAD_GRAYSCALE)
+                resized_image = cv2.resize(img1, (100, 100))
+                cv2.imwrite(current_image_path, resized_image)
 
-        current_image_path = "gun.jpg"
-        img1 = cv2.imread(current_image_path, cv2.IMREAD_GRAYSCALE)
-        resized_image = cv2.resize(img1, (315, 180))
-        cv2.imwrite(current_image_path, resized_image)
+        # current_image_path = "gun.jpg"
+        # img1 = cv2.imread(current_image_path, cv2.IMREAD_GRAYSCALE)
+        # resized_image = cv2.resize(img1, (50, 50))
+        # cv2.imwrite(current_image_path, resized_image)
 
 resize()
